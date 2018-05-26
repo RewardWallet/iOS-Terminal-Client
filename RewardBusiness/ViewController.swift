34a131e14b5d4e9ab2e0401bd1a2c64944827c35
bmolly <bmolly94@hotmail.com>
2018-05-25 22:43:41 -0700

    Initial Commit

---
+++ RewardBusiness/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RewardBusiness
+//
+//  Created by MOLLY BIN on 2018-05-25.
+//  Copyright © 2018 MOLLY BIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
